83e253903b281f99dd7bf0b3d9e24e5298f05870
Luka Pavlovic <92918995+LukaPavlovicGit@users.noreply.github.com>
2022-12-01 17:57:08 +0100

    Update app_crud.js

--- app_crud.js
+++ app_crud.js
@@ -5,8 +5,6 @@ const path = require('path');
 const cors = require('cors');
 
 const users = require('./routes/users_route');
-//const restaurants = require('./routes/restaurants_route');
-//const foods = require('./routes/foods_route');
 const comments = require('./routes/comments_route');
 
 app.use(express.json());
