ee98f3d6c65cc378ac9cabaee0bc740140c1df19
Luka <lpavlovic11521rn@raf.rs>
2022-12-05 19:19:44 +0100

    added gui_app, index.html and index.js

---
+++ static/scripts/index.js
@@ -0,0 +1,28 @@
+window.addEventListener('load', init);
+
+function init() {
+    document.getElementById('logout-button').addEventListener('click', e => {
+        document.cookie = `token=;SameSite=Lax`;
+        window.location.href = '/login';
+    })
+
+    document.getElementById('user-button').addEventListener('click', e => {
+        window.location.href = '/users';
+    })
+
+    document.getElementById('article-button').addEventListener('click', e => {
+        window.location.href = '/articles';
+    })
+
+    document.getElementById('delivery-button').addEventListener('click', e => {
+        window.location.href = '/deliveries';
+    })
+
+    document.getElementById('invoice-button').addEventListener('click', e => {
+        window.location.href = '/invoices';
+    })
+
+    document.getElementById('comment-button').addEventListener('click', e => {
+        window.location.href = '/comments';
+    })
+}
